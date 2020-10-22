8235c3a5711b5ab1a024881bb2256fd93a0e9450
heatherzen <heather.zen@gmail.com>
2020-10-22 16:29:52 -0700

    got the links working and added about para and headshot

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import coverImage from "../../assets/images/cover-image/cover-photo.jpg";
+import headShot from "../../assets/images/headshot/profile-pic3.jpg";
 
 const About = () => {
     return (
@@ -7,8 +8,9 @@ const About = () => {
             
             <section id='about' className='about'>
                 <h1 className='title' style={{ color: 'blueviolet' }}>Heather Graham</h1>
+                <img src={headShot} className='head-shot' style={{ width: '30%', height: '30%' }} alt='profile picture'/>
                 <img src={coverImage} className='cover-image' style={{  width: '100%', height: '100%' }} alt='coding imagery'/>
-                <p className='about-para'>Hi this is my about me section</p>
+                <p className='about-para'>I am Heather Graham.  A new, up and coming developer with Full-Stack skills.  I aspire to get into the fields of App development for both IOS and Android.  In my spare time, I enjoy doing henna art and playing guitar.</p>
             </section>
             
             
--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -9,42 +9,48 @@ function PhotoList({ category }) {
       category: 'projects',
       description: 'A git repo search engine',
       image: 'git-it-done',
-      url: 'https://heatherzen.github.io/git-it-done/'
+      url: 'https://heatherzen.github.io/git-it-done/',
+      url2: 'https://github.com/heatherzen/git-it-done'
     },
     {
       name: 'Rv Roadtrip',
       category: 'projects',
       description: 'A website for RV camping grounds in national parks',
       image: 'rv-roadtrip',
-      url: 'https://heatherzen.github.io/ZitasRv-Roadtrip/'
+      url: 'https://heatherzen.github.io/ZitasRv-Roadtrip/',
+      url2: 'https://github.com/heatherzen/ZitasRv-Roadtrip'
     },
     {
       name: 'My Schtick Kick',
       category: 'projects',
       description: 'A hobby social media site',
       image: 'my-schtick-kick',
-      url: 'https://fathomless-waters-65385.herokuapp.com/'
+      url: 'https://fathomless-waters-65385.herokuapp.com/',
+      url2: 'https://github.com/heatherzen/myschtickkick'
     },
     {
       name: 'My thoughts',
       category: 'projects',
       description: 'a backend social media application',
       image: 'my-thoughts',
-      url: 'https://github.com/heatherzen/my-thoughts'
+      url: 'https://github.com/heatherzen/my-thoughts',
+      url2: 'https://github.com/heatherzen/my-thoughts'
     },
     {
       name: 'Password Generator',
       category: 'projects',
       description: 'a password generator',
       image: 'password-generator',
-      url: 'https://heatherzen.github.io/heatherpwgen/'
+      url: 'https://heatherzen.github.io/heatherpwgen/',
+      url2: 'https://github.com/heatherzen/heatherpwgen'
     },
     {
       name: 'Run Buddy',
       category: 'projects',
       description: 'a fitness website',
       image: 'run-buddy',
-      url: 'https://heatherzen.github.io/run-buddy/'
+      url: 'https://heatherzen.github.io/run-buddy/',
+      url2: 'https://github.com/heatherzen/run-buddy'
     },
   ]);
   const currentPhotos = photos.filter((photo) => photo.category === category);
@@ -56,24 +62,27 @@ function PhotoList({ category }) {
         {currentPhotos.map((image) => {
           // console.log(image, url);
           return (
-            <a href={image.url}
-            onClick={(event) => {
-              event.preventDefault();
-              console.log(image.url);
-              return(image.url);
-          }}
-            >
+            <div className="image-links">
+              <h3 className="image-title">{image.name}</h3>
               <img
                 src={require(`../../assets/images/${category}/${image.image}.png`)}
                 alt={image.name}
                 className="img-thumbnail mx-1"
                 key={image.name}
               />
-            </a>
+              <div>
+                <a href={image.url}>Click here for to view Website</a>
+              </div>
+              <div>
+                <a href={image.url2}>Click here for Github code</a>
+              </div>
+            </div>
+
           )
         })}
       </div>
     </div>
+
   );
 }
 
