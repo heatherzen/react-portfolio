6697a6bc13d82428cb6edabb7ee74c65cdc212d0
heatherzen <heather.zen@gmail.com>
2021-01-10 11:42:36 -0700

    changed some css for name

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ function Header(props) {
 
   return (
     <header className="flex-row space-between px-1 text-color">
-      <h1>Heather Graham</h1>
+      <h1 className="name">Heather Graham</h1>
       {props.children}
     </header>
   );
